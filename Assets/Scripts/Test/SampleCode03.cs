e98d117b95d0fee43579d7bdc40837d7c5fb2339
NK Studio <53283888+NK-Studio@users.noreply.github.com>
2024-03-20 16:33:37 +0900

    [chore] 로컬 푸시 구현

---
+++ Assets/Scripts/Test/SampleCode03.cs
@@ -0,0 +1,70 @@
+using System;
+using Unity.Notifications.Android;
+using UnityEngine;
+
+public class SampleCode03 : MonoBehaviour
+{
+    private static int id;
+    private const string ChannelID = "LG_Channel_id";
+
+    /// <summary>
+    /// 노티피케이션을 3초후 울립니다.
+    /// </summary>
+    public void TestSend()
+    {
+        DateTime currentTime = DateTime.Now;
+        //DateTime targetTime = currentTime.AddSeconds(3f);
+
+        SendNotification("LG 테스트", "보상을 받아라!!!", currentTime, true);
+    }
+
+    /// <summary>
+    /// 알림을 전달합니다.
+    /// </summary>
+    /// <param name="title">제목</param>
+    /// <param name="message">내용</param>
+    /// <param name="time">전달 시간</param>
+    /// <param name="test">앱이 켜져있어도 울릴 것인가?</param>
+    public static void SendNotification(string title, string message, DateTime time, bool test = false)
+    {
+#if UNITY_ANDROID
+        NotificationStatus notificationStatus = AndroidNotificationCenter.CheckScheduledNotificationStatus(id);
+
+        Debug.Log("PPAP : " + notificationStatus);
+        if (notificationStatus == NotificationStatus.Unknown)
+        {
+            var channel = new AndroidNotificationChannel() {
+                Id = ChannelID,
+                Name = "LG Project",
+                Importance = Importance.Default,
+                Description = "기본 알림"
+            };
+            
+            AndroidNotificationCenter.RegisterNotificationChannel(channel);
+
+            AndroidNotification notification = new() {
+                Title = title,
+                Text = message,
+                SmallIcon = "lg_small",
+                LargeIcon = "lg_large",
+                ShowInForeground = test,
+                FireTime = time
+            };
+
+            AndroidNotificationCenter.SendNotification(notification, ChannelID);
+        }
+#endif
+    }
+
+    /// <summary>
+    /// 알림을 모두 취소합니다.
+    /// </summary>
+    public static void CancelAllNotifications()
+    {
+#if UNITY_ANDROID
+        AndroidNotificationCenter.CancelAllNotifications();
+#endif
+    }
+
+
+}
