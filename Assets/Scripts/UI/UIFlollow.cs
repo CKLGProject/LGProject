75f9608a061acab8cd2a0c55940a9db523adcceb
NK Studio <NK-Studio@users.noreply.github.com>
2024-06-13 12:30:44 +0900

    [fixed] 카메라 이동은 LateUpdate에서 진행을 해야한다.

--- Assets/Scripts/UI/UIFlollow.cs
+++ Assets/Scripts/UI/UIFlollow.cs
@@ -1,27 +1,32 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class UIFollow : MonoBehaviour
 {
-    public Transform target; // 따라다닐 대상
-    public Vector3 offset; // 대상과의 위치 오프셋
-    private RectTransform uiElement; // UI 요소의 RectTransform
+    [FormerlySerializedAs("target")] public Transform Target; // 따라다닐 대상
+    [FormerlySerializedAs("offset")] public Vector3 Offset; // 대상과의 위치 오프셋
+    private RectTransform _rectTransform; // UI 요소의 RectTransform
 
-    void Start()
+    private Camera _mainCamera;
+    
+    private void Start()
     {
+        _mainCamera = Camera.main;
+        
         // UI 요소의 RectTransform을 가져옵니다.
-        uiElement = GetComponent<RectTransform>();
-        target = GameObject.Find("Player").transform;
+        _rectTransform = GetComponent<RectTransform>();
+        Target = GameObject.Find("Player").transform;
     }
 
-    void Update()
+    private void LateUpdate()
     {
-        if (target != null)
+        if (Target)
         {
             // 대상의 월드 좌표를 스크린 좌표로 변환합니다.
-            Vector3 screenPosition = Camera.main.WorldToScreenPoint(target.position + offset);
+            Vector3 screenPosition = _mainCamera.WorldToScreenPoint(Target.position + Offset);
 
             // UI 요소의 위치를 스크린 좌표로 설정합니다.
-            uiElement.position = screenPosition;
+            _rectTransform.position = screenPosition;
         }
     }
 }
