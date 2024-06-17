26435d42aeb1594eada7d27c784ea66cc62670b7
NK Studio <NK-Studio@users.noreply.github.com>
2024-06-17 15:54:25 +0900

    [update] 더블 탭 전역 변경 & 제스처 최종 완료

--- Assets/Scripts/Utility/Runtime/DoubleTap Controller.cs
+++ Assets/Scripts/Utility/Runtime/DoubleTap Controller.cs
@@ -1,17 +1,46 @@
 using R3;
-using ReactiveTouchDown;
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DoubleTapController : MonoBehaviour
 {
-    [SerializeField] private GameObject touchArea;
+    private Subject<Unit> _onTouchDown;
     
+    private InputAction _touchPrimaryDoubleTap;
+
+    private void Awake()
+    {
+        _onTouchDown = new Subject<Unit>();
+        _touchPrimaryDoubleTap = InputSystem.actions.FindActionMap("System").FindAction("Double Tap");
+    }
+
+    private void Start()
+    {
+        _touchPrimaryDoubleTap.performed += OnDoubleTouch;
+    }
+
+    private void OnEnable()
+    {
+        _touchPrimaryDoubleTap.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _touchPrimaryDoubleTap.Disable();
+    }
+    
+    private void OnDoubleTouch(InputAction.CallbackContext obj)
+    {
+        _onTouchDown.OnNext(Unit.Default);
+    }
+
     /// <summary>
     /// 더블 터치를 인식하는 옵저버입니다.
     /// </summary>
     /// <returns></returns>
     public Observable<Unit> OnDoubleTouchObservable()
     {
-        return touchArea.DoubleTouchDownAsObservable();
+        return _onTouchDown.AsObservable();
     }
 }
